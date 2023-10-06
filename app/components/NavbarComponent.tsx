36558cb857e39c7a99f0625d462a4242b1d7bf55
Hung Pham <mryo.hp@gmail.com>
2023-10-06 01:05:31 -0700

    Config themes

--- app/components/NavbarComponent.tsx
+++ app/components/NavbarComponent.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from "../../public/images/h_logo.png"
-
+import logo_white from "../../public/images/h_logo_w.png"
 export default function NavbarComponent() {
     return (
         <div>
@@ -33,10 +33,8 @@ export default function NavbarComponent() {
                     </div>
                     {/* Logo */}
                     <Link href="/" className='lg:ml-36 ml-4'>
-                        <Image src={logo} alt="avatar" width={640} height={256} className="w-48 h-14" />
+                        <Image src={logo} alt="avatar" width={640} height={256} className="w-48 h-14 logo-image" />
                     </Link>
-                    {/* End Logo */}
-                  
                 </div>
                 {/* Main Menu */}
                 <div className="navbar-center hidden lg:flex">
@@ -118,7 +116,7 @@ export default function NavbarComponent() {
                     {/* Toggle darkmode button */}
                     <label className="swap swap-rotate mr-4">
                         {/* this hidden checkbox controls the state */}
-                        <input type="checkbox" data-toggle-theme="dark" />
+                        <input type="checkbox" data-toggle-theme="dark,cupcake" />
                         {/* sun icon */}
                         <svg className="swap-on fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
                         {/* moon icon */}
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-theme="dark">
+         
       <body className={inter.className}>
         <Navbar />
         {children}
