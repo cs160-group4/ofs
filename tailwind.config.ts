854daaee710ececb823d69316d3f832982d32713
Hung Pham <mryo.hp@gmail.com>
2023-10-06 00:11:48 -0700

    modify logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark"],
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
