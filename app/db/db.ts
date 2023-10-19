8bf478dfacf22957a5ccf1f117ab17b6968048b1
Kyle Chen <chenchhaykyle@gmail.com>
2023-10-18 17:25:50 -0700

    alkjf

--- app/db/db.ts
+++ app/db/db.ts
@@ -2,6 +2,7 @@ import * as schema from './schema';
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
+dotenv.config({ path: ".env.local"});
 const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
