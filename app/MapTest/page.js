3c6f0b1cc851b4f1f08cd4e09726ea43463ff739
Nandish Kumar <100751595+Nandish-Kumar-SJSU@users.noreply.github.com>
2023-10-25 15:01:28 -0700

    Map component added
    Map component works on test, need to figure out how to add it to dashboard and client side. Image on icon doesnt work either. Hard coded data currently. need to add address from data base and geocoding

---
+++ app/MapTest/page.js
@@ -0,0 +1,11 @@
+'use client';
+import MapComponent from "../components/MapComponent";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Map with routing</h1>
+            <MapComponent/>
+        </div>
+    );
+}
