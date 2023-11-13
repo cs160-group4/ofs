f5d4fcbbe52979de803521351fffecb31d633172
Nandish Kumar <100751595+Nandish-Kumar-SJSU@users.noreply.github.com>
2023-11-13 15:15:58 -0800

    fetch user id

--- app/(home)/MapTest/page.tsx
+++ app/(home)/MapTest/page.tsx
@@ -6,7 +6,7 @@ import { getOrdersWithAddressesByUserId } from "@/lib/orders"; // import your fu
 const MapPage = () => {
     const [startPoint, setStartPoint] = useState<[number, number]>([0, 0]);
     const [endPoint, setEndPoint] = useState<[number, number]>([0, 0]);
-    const userId = "user123";
+    const userId = ""; // fetch user 
 
     useEffect(() => {
         const fetchData = async () => {
