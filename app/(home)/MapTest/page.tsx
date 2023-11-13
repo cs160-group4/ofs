35fbd2b599be53172be1929909dec8d76c613dcd
Nandish Kumar <100751595+Nandish-Kumar-SJSU@users.noreply.github.com>
2023-11-13 15:15:10 -0800

    map component

--- app/(home)/MapTest/page.tsx
+++ app/(home)/MapTest/page.tsx
@@ -1,13 +1,37 @@
-import MapComponent from "@/components/MapComponent";
-import { getOrdersWithAddresses } from "@/lib/orders";
-export default async function MapTest() {
-    const list = await getOrdersWithAddresses();
+'use client'
+import React, { useEffect, useState } from 'react';
+import MapComponent from '@/components/MapComponent';
+import { getOrdersWithAddressesByUserId } from "@/lib/orders"; // import your function
+
+const MapPage = () => {
+    const [startPoint, setStartPoint] = useState<[number, number]>([0, 0]);
+    const [endPoint, setEndPoint] = useState<[number, number]>([0, 0]);
+    const userId = "user123";
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const orders = await getOrdersWithAddressesByUserId(userId);
+                if (orders.length > 0) {
+                    const latestOrder = orders[0];
+
+                    setEndPoint([latestOrder.addresses.longitude, latestOrder.addresses.latitude]);
+
+                    setStartPoint([37.3352, -121.8811]); // Example start point
+                }
+            } catch (error) {
+                console.error('Error fetching order data:', error);
+            }
+        };
+
+        fetchData();
+    }, [userId]);
+
     return (
-        <>
-            <div>
-                <h1>Map with routing</h1>
-                <MapComponent  list={list}/>
-            </div>
-        </>
+        <div>
+            <MapComponent startPoint={startPoint} endPoint={endPoint} />
+        </div>
     );
-}
+};
+
+export default MapPage;
