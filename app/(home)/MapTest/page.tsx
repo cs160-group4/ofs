a66d10d4a034a005ff9fe686d90588f7985d6c15
Nandish Kumar <100751595+Nandish-Kumar-SJSU@users.noreply.github.com>
2023-11-15 14:15:33 -0800

    map for customers
    map for customers

--- app/(home)/MapTest/page.tsx
+++ app/(home)/MapTest/page.tsx
@@ -1,13 +1,38 @@
+import React from 'react';
 import MapComponent from "@/components/MapComponent";
-import { getOrdersWithAddresses } from "@/lib/orders";
+import { getOrdersWithAddressesByUserId } from "@/lib/orders";
+import { getAuthSession } from "@/api/auth/[...nextauth]/options";
+
 export default async function MapTest() {
-    const list = await getOrdersWithAddresses();
-    return (
-        <>
-            <div>
-                <h1>Map with routing</h1>
-                <MapComponent  list={list}/>
-            </div>
-        </>
-    );
+    let signedIn = false;
+    let name = '';
+    let id = '';
+
+    const session = await getAuthSession();
+    if (session?.user) {
+        signedIn = true;
+        name = typeof session.user.name === 'string' ? session.user.name : '';
+        id = typeof session.user.id === 'string' ? session.user.id : '';
+    }
+
+    if (!id) {
+        // Handle the scenario where the ID is not available
+        console.error('User ID is undefined.');
+        return;
+    }
+
+    try {
+        const list = await getOrdersWithAddressesByUserId(id);
+        return (
+            <>
+                <div>
+                    <h1>Map with routing</h1>
+                    <MapComponent list={list}/>
+                </div>
+            </>
+        );
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        // Return an error component or handle the error appropriately
+    }
 }
