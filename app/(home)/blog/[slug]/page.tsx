82e14189fca62d19bfd266a00200516a1e9f4445
Hung Pham <mryo.hp@gmail.com>
2023-12-04 21:26:44 -0800

    Fix: Blog post user’s avatar (Customer)

--- app/(home)/blog/[slug]/page.tsx
+++ app/(home)/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { Post, getPostById } from '@/app/lib/posts';
+import { getAvatarURL } from '@/app/lib/utils';
 import { not } from 'drizzle-orm';
 import Image from 'next/image'
 import { notFound } from 'next/navigation';
@@ -15,16 +16,17 @@ export default async function BlogPage({ params }: { params: { slug: string } })
   if (!list) return notFound();
   let post: Post = list.blog_posts;
   let user = list.user;
+  let image = getAvatarURL(user.image);
 
   return (
     <>
       <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
-          <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+          <article className="mx-auto w-full max-w-2xl ">
             <header className="mb-4 lg:mb-6 not-format">
               <address className="flex items-center mb-6 not-italic">
                 <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <Image src="" width={36} height={36} className="mr-4 w-16 h-16 rounded-full" alt="" />
+                  <Image src={image} width={36} height={36} className="mr-4 w-16 h-16 rounded-full" alt="" />
                   <div>
                     <a href="#" rel="author" className="text-xl font-bold text-gray-900 dark:text-white">{user.name}</a>
                     <p className="text-base text-gray-500 dark:text-gray-400">{user.role}</p>
@@ -32,11 +34,11 @@ export default async function BlogPage({ params }: { params: { slug: string } })
                   </div>
                 </div>
               </address>
-              <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+              <h1 className="mb-4 text-3xl  text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
                 {post.title}
               </h1>
             </header>
-            <p className="lead">
+            <p className="text-gray-900">
               {post.content}
             </p>
 
